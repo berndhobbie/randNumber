6483fd8cee0a9c03a60dc8c6bd80f4e854ff7507
Bernd Hobbie <LaleLuDev@users.noreply.github.com>
2023-09-04 13:11:36 +0200

    it looks cool

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -1,1 +1,15 @@
-<h2>Moin Moin</h2>
+<script lang="ts">
+  let currentNumber = 0;
+  const getRandomNumber = () => {
+    const newNumber = Math.floor(Math.random() * 75) + 1;
+    currentNumber = newNumber;
+  };
+</script>
+
+<div class="flex flex-col items-center justify-center text-white font-mono">
+  <h2 class="text-9xl">{currentNumber}</h2>
+  <button
+    class="border-2 border-orange-500 p-2 text-2xl hover:bg-slate-800"
+    on:click={getRandomNumber}>Rand Number</button
+  >
+</div>
